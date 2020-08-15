ec2012ca4db92f6688536b01c1e0c2d131cd60f7
Travis CI <travis@travis-ci.org>
2020-08-15 19:07:58 +0300

    Chat Improvements
    - Chat is now scaling based on the length of the text.

---
+++ Assets/Scripts/Scene/UI/PlayerProfilePrivate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+using Mirror;
+
+public class PlayerProfilePrivate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TMP_Text PlayerName;
+    public TMP_Text Coins;
+
+    void Start()
+    {
+        GetPlayerProfileRequest request = new GetPlayerProfileRequest();
+        PlayFabClientAPI.GetPlayerProfile(request, Success =>
+        {
+            PlayerName.text = Success.PlayerProfile.DisplayName;
+            GetUserInventoryRequest request2 = new GetUserInventoryRequest();
+            PlayFabClientAPI.GetUserInventory(request2, success =>
+            {
+                int Currency;
+                success.VirtualCurrency.TryGetValue("CO", out Currency);
+                Coins.text = "Balance: " + Currency.ToString();
+
+
+
+            }, fail2 => { Start(); });
+
+        }, fail => { Start(); });
+    }
+}
