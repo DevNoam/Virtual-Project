96cf7092f32b1dfc83c3deb532f7ada10b039463
sapirnoam <42315483+sapirnoam@users.noreply.github.com>
2020-09-29 22:34:17 +0300

    Revert "Assets arrangement"
    This reverts commit 5cdb550041c03fda70c1e29fbc2eee1a5a6d0adc.

---
+++ Assets/Inventory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class Inventory : MonoBehaviour
+{
+    void start()
+    { }
+}
