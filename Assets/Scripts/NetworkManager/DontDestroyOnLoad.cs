07acab234125df6059f6d4f725aa117ba4a49a93
sapirnoam <42315483+sapirnoam@users.noreply.github.com>
2021-02-12 20:22:35 +0200

    Quality of life update - WIP
    - PlayerManager Script has been updated, This is stilstill WIP and contains problems.
    
    - RoomManager replaced by PlayFabPlayer.cs, which now handles all the PlayFab requests ingame.
    
    - Player prefab has been Improved.
    
    - Now when a new Client joins a game, there is no need to request from all the Active clients to broadcast their locations, their positions automaticly syncing.
    
    - Project folder has been Organized.

--- Assets/Scripts/NetworkManager/DontDestroyOnLoad.cs
+++ Assets/Scripts/NetworkManager/DontDestroyOnLoad.cs
@@ -7,8 +7,6 @@ public class DontDestroyOnLoad : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    PlayerManager playerManagerLocal;
-
     void Start()
     {
         DontDestroyOnLoad(this.gameObject);
