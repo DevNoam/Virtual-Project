3afd806c78983bdb2787b834556efdc6c38f7ce3
sapirnoam <42315483+sapirnoam@users.noreply.github.com>
2021-02-15 22:39:52 +0200

    GUI Improvements FINAL
    - Added Noam3D Intro

---
+++ Assets/Scripts/MoveToMainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+public class MoveToMainMenu : MonoBehaviour
+{
+    public VideoPlayer vid;
+
+
+    void Start() {
+        if (Application.isBatchMode)
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        vid.loopPointReached += CheckOver; 
+    
+    }
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
